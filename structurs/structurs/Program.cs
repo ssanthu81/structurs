8c04155e7833551ebf1940af2c1cb4a678fa2f8a
ssanthu81 <101133033+ssanthu81@users.noreply.github.com>
2022-05-13 09:10:16 -0500

    Add files via upload

---
+++ structurs/structurs/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    int i;
+    public  void display()
+    {
+        Console.WriteLine("first");
+    }
+    static void Main()
+    {
+        Program p = new Program();
+        p.i = 20;
+        p.display();
+        Console.ReadLine();
+    }
+
+}
